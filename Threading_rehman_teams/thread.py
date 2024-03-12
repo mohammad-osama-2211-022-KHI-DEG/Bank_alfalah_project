a71b11c554caee7503aca7781f48b79503617067
hamzaasim3639 <hamza.asim@xloopdigital.com>
2024-03-12 15:03:20 +0500

    Update thread.py
    Logging implementation / refactoring

--- Threading_rehman_teams/thread.py
+++ Threading_rehman_teams/thread.py
@@ -1,4 +1,3 @@
-import cv2
 import logging
 from ultralytics import YOLO
 from collections import defaultdict
@@ -6,32 +5,31 @@ from datetime import datetime, timezone, timedelta
 import threading
 import httpx
 import requests
+import cv2
 import queue
-
-from atm_functionality_detection.main_atm import process_frame, complainbox_telephone, Time, data_preparation, Alert
-from suspecious_activity.main_suspecious import process_frame_thread, detect_suspicious_conditions, check_person_duration, load_model
+from atm_functionality_detection.main import process_frame
+from suspecious_activity.main import process_frame_thread
 from guard.main_guard import process_guard_frame_thread
-#from atm_functionality_detection.main import *
+
 
 # For video
 VIDEO_PATH = '../team_raqim_rehman/atm_functionality_detection/ATM_working.mp4'
 
 
 # For Streaming
-# username = 'usama.xloop'
-# password = 'Xloop@123'
-# ip_address = '192.168.6.19'
-# stream_url = f"rtsp://{username}:{password}@{ip_address}:554/cam/realmonitor?channel=1&subtype=1"
+username = 'usama.xloop'
+password = 'Xloop@123'
+ip_address = '192.168.6.19'
+stream_url = f"rtsp://{username}:{password}@{ip_address}:554/cam/realmonitor?channel=1&subtype=1"
+
 
 model_path_atm_url = '../team_raqim_rehman/atm_functionality_detection/new_best.pt'
 model_suspecious_url = '../team_raqim_rehman/suspecious_activity/suspecious_updated.pt'
-model_guard_detection_url = '../team_raqim_rehman/guard/guard_attire.pt'
-model_guard_features_url = '../team_raqim_rehman/guard/guard_features.pt'
+
 
 model_suspecious = YOLO(model_suspecious_url)
 model_atm = YOLO(model_path_atm_url)
-model_guard_detection = YOLO(model_guard_detection_url)
-model_guard_features = YOLO(model_guard_features_url)
+
 
 
 LOG_FILENAME = '../team_raqim_rehman/thread_log.log'
@@ -42,6 +40,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
+logger = logging.getLogger(__name__)
 # Define HTTP headers and URL
 headers = {
     "Content-Type": "application/json",
@@ -50,7 +49,6 @@ headers = {
     "X-SERVER-TO-SERVER" : "true"
 }
 url = 'http://13.235.71.140:5000/atm-functionality'
-
 # Initialize variables
 atm_detected = False
 total_working_count = 0
@@ -58,25 +56,29 @@ total_notworking_count = 0
 person = False
 counter = 0
 count = 0
-
 MAX_QUEUE_SIZE = 100  # Define your maximum queue size
 frame_queue = queue.Queue(maxsize=MAX_QUEUE_SIZE)
 
+
 def main():
+
     """
     Main function to start the face detection and recognition process on a video file.
     """
     # Open the video capture
     video_capture = cv2.VideoCapture(VIDEO_PATH)
     fps = int(video_capture.get(cv2.CAP_PROP_FPS))
-    logging.info(f"frame_rate: {fps}")
+    logger.info(f"frame_rate: {fps}")
 
     # Target processing interval for 10 fps
-    FPS_INTERVEL = 10
+    TARGET_FPS = 3
+    PROCESS_INTERVAL = int(fps / TARGET_FPS)
     frame_count = 0
 
+    logger.info(f"PROCESS_INTERVAL: {PROCESS_INTERVAL}")
+
     try:
-        logging.info("Streaming started")
+        logger.info("Streaming started")
         print("Streaming started")
 
         # Loop through the video frames
@@ -86,21 +88,28 @@ def main():
                 break
             
             frame_count += 1
-            if frame_count % FPS_INTERVEL != 0:
-                logging.info(f"frame : {frame_count % FPS_INTERVEL}")
-                continue
-            # Put the frame into the queue
-            try:
-                frame_queue.put_nowait(frame)
-            except queue.Full:
-                logging.warning("Frame queue is full, dropping frames.")
-            process_frames()
+
+
+            # Process only the selected frames based on the interval
+            if frame_count % PROCESS_INTERVAL == 0:
+
+                # Determine the current frame number
+                current_frame_number = int(video_capture.get(cv2.CAP_PROP_POS_FRAMES))
+                logger.info(f"current frame number: {current_frame_number}")
+                # Put the frame into the queue
+                try:
+                    frame_queue.put_nowait(frame)
+                except queue.Full:
+                    logger.warning("Frame queue is full, dropping frames.")
+                process_frames()
 
     except Exception as e:
-        logging.error(f"An error occurred in the main function: {e}")
+        logger.error(f"An error occurred in the main function: {e}")
     finally:
         # Release the video capture
         video_capture.release()
+
+
 
 # Function to process frames
 def process_frames():
@@ -113,7 +122,7 @@ def process_frames():
             # Process the frame
             t1 = threading.Thread(target=process_frame, args=(frame, model_atm, atm_detected, total_working_count, total_notworking_count, counter, url, headers))
             t2 = threading.Thread(target=process_frame_thread, args=(model_suspecious, frame, first_person_time))
-            t3 = threading.Thread(target=process_guard_frame_thread, args=(model_guard_detection, model_guard_features, frame, guard_attire_statuses, previous_guard_attire_statuses))
+            t3 = threading.Thread(target=process_guard_frame_thread, args=( frame, guard_attire_statuses, previous_guard_attire_statuses))
             t1.start()
             t2.start()
             t3.start()
@@ -122,9 +131,7 @@ def process_frames():
             t3.join()
         else:
             break
-
-
-
 if __name__ == '__main__':
     # Start the frame processing thread
     main()
+
