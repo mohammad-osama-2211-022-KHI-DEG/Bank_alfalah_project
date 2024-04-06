c7b1c68ec14ab30416ff6b8b72851c2543760f5d
hamzaasim3639 <hamza.asim@xloopdigital.com>
2024-04-06 13:14:47 +0500

    Update data_collection.py

--- Threading_rehman_teams/data_collection.py
+++ Threading_rehman_teams/data_collection.py
@@ -2,13 +2,13 @@ import cv2
 from ultralytics import YOLO
 import os
 
-# Function to process a single frame's results and save them
+# ------------------------------------FUNCTION TO BE USED FOR ANNOTATION----------------------------------------------
 def annotate_frame(frame_count, frame, results):
     if len(results[0].boxes.xyxyn) > 0:
         # Create directories if they don't exist
         os.makedirs('./images/', exist_ok=True)
         os.makedirs('./labels/', exist_ok=True)
-
+        
         image_filename = f'./images/frame_{frame_count}.jpg'
         label_filename = f'./labels/frame_{frame_count}.txt'
 
@@ -18,14 +18,17 @@ def annotate_frame(frame_count, frame, results):
         # Save labels
         with open(label_filename, 'w') as file:
             for result in results:
-                for i in range(len(result.boxes.xyxy)):
-                    file.write(f'{result.names[i]} {result.boxes.xyxyn[i][0]} {result.boxes.xyxyn[i][1]} {result.boxes.xyxyn[i][2]} {result.boxes.xyxyn[i][3]}\n')
-
+                boxes = result.boxes
+                for box in boxes:
+                    b = box.xyxyn  # get box coordinates in (left, top, right, bottom) format
+                    c = box.cls
+                    file.write(f'{model.names[int(c)]} {b[0][0]} {b[0][1]} {b[0][2]} {b[0][3]}\n')
+# ------------------------------------FUNCTION TO BE USED FOR ANNOTATION----------------------------------------------
 
 
 # Initialize YOLO model and video capture
-model = YOLO('new_best.pt')
-video_path = 'ATM_working.mp4'  # Replace with your video path
+model = YOLO('atm_cleanliness_best.pt')
+video_path = 'videos/cleanliness.mp4'  # Replace with your video path
 cap = cv2.VideoCapture(video_path)
 
 frame_count = 0
@@ -35,10 +38,16 @@ while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
-
+   
     # Detect objects in the frame
     results = model.predict(frame)
-
+    for result in results:
+        boxes = result.boxes
+        for box in boxes:
+            b = box.xyxyn  # Convert tensor to list
+            c = box.cls
+            print(f' {model.names[int(c)]} {b[0][0]} {b[0][1]} {b[0][2]} {b[0][3]}')
+            
     # Process the frame results
     annotate_frame(frame_count, frame, results)
 
